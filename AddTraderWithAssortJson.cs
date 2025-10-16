f861c4c848c996110e2436f66a5b4f4987931faa
Tiltushkin <ivlad6543@gmail.com>
2025-10-16 18:26:57 +0500

    chore: removed unused .sln and change mod Url

--- AddTraderWithAssortJson.cs
+++ AddTraderWithAssortJson.cs
@@ -23,7 +23,7 @@ public record ModMetadata : AbstractModMetadata
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
     public override List<string>? Incompatibilities { get; init; } = [""];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; } = "https://github.com/Tiltushkin";
+    public override string? Url { get; init; } = "https://github.com/Tiltushkin/Tiltushkin-AllAmmo-CSharp/";
     public override bool? IsBundleMod { get; init; } = false;
     public override string? License { get; init; } = "MIT";
 }
