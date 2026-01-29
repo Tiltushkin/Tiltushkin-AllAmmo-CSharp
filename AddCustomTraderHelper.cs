503fae02ffaaa30b88647abf7f12bf8418ac03e9
Tiltushkin <ivlad6543@gmail.com>
2026-01-29 13:43:51 +0500

    feat: debugging mod errors
    Added error logging to the mod, now you can describe errors with the mod in more detail (e.g., conflicts with other mods)

--- AddCustomTraderHelper.cs
+++ AddCustomTraderHelper.cs
@@ -9,9 +9,6 @@ using SPTarkov.Server.Core.Utils.Cloners;
 
 namespace _allAmmo
 {
-    /// <summary>
-    /// We inject this class into 'AddTraderWithDynamicAssorts' to help us with adding the new trader into the server
-    /// </summary>
     [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
     public class AddCustomTraderHelper(
         ISptLogger<AddCustomTraderHelper> logger,
@@ -19,17 +16,8 @@ namespace _allAmmo
         DatabaseService databaseService,
         LocaleService localeService)
     {
-
-        /// <summary>
-        /// Add the traders update time for when their offers refresh
-        /// </summary>
-        /// <param name="traderConfig">trader config to add our trader to</param>
-        /// <param name="baseJson">json file for trader (db/base.json)</param>
-        /// <param name="refreshTimeSecondsMin">How many seconds between trader stock refresh min time</param>
-        /// <param name="refreshTimeSecondsMax">How many seconds between trader stock refresh max time</param>
         public void SetTraderUpdateTime(TraderConfig traderConfig, TraderBase baseJson, int refreshTimeSecondsMin, int refreshTimeSecondsMax)
         {
-            // Add refresh time in seconds to config
             var traderRefreshRecord = new UpdateTime
             {
                 TraderId = baseJson.Id,
@@ -39,13 +27,8 @@ namespace _allAmmo
             traderConfig.UpdateTime.Add(traderRefreshRecord);
         }
 
-        /// <summary>
-        /// Add a traders base data to the server, no assort items
-        /// </summary>
-        /// <param name="traderDetailsToAdd">trader details</param>
         public void AddTraderWithEmptyAssortToDb(TraderBase traderDetailsToAdd)
         {
-            // Create an empty assort ready for our items
             var emptyTraderItemAssortObject = new TraderAssort
             {
                 Items = [],
@@ -53,14 +36,12 @@ namespace _allAmmo
                 LoyalLevelItems = new Dictionary<MongoId, int>()
             };
 
-            // Create trader data ready to add to database
             var traderDataToAdd = new Trader
             {
                 Assort = emptyTraderItemAssortObject,
                 Base = cloner.Clone(traderDetailsToAdd),
-                QuestAssort = new() // quest assort is empty as trader has no assorts unlocked by quests
+                QuestAssort = new()
                 {
-                    // We create 3 empty arrays, one for each of the main statuses that are possible
                     { "Started", new() },
                     { "Success", new() },
                     { "Fail", new() }
@@ -68,22 +49,14 @@ namespace _allAmmo
                 Dialogue = []
             };
 
-            // Add the new trader id and data to the server
             if (!databaseService.GetTables().Traders.TryAdd(traderDetailsToAdd.Id, traderDataToAdd))
             {
-                //Failed to add trader!
+                logger.Error($"[AllAmmo] FAILED to add trader: {traderDetailsToAdd.Id}. This ID is already in use by another mod! Change the _id in base.json.");
             }
         }
 
-        /// <summary>
-        /// Add traders name/location/description to all locales (e.g. German/French/English)
-        /// </summary>
-        /// <param name="baseJson">json file for trader (db/base.json)</param>
-        /// <param name="firstName">First name of trader</param>
-        /// <param name="description">Flavor text of whom the trader is</param>
         public void AddTraderToLocales(TraderBase baseJson, string firstName, string description)
         {
-            // For each language, add locale for the new trader
             var locales = databaseService.GetTables().Locales.Global;
             var newTraderId = baseJson.Id;
             var fullName = baseJson.Name;
@@ -92,35 +65,26 @@ namespace _allAmmo
 
             foreach (var (localeKey, localeKvP) in locales)
             {
-                // We have to add a transformer here, because locales are lazy loaded due to them taking up huge space in memory
-                // The transformer will make sure that each time the locales are requested, the ones added below are included
                 localeKvP.AddTransformer(lazyloadedLocaleData =>
                 {
-                    lazyloadedLocaleData.Add($"{newTraderId} FullName", fullName);
-                    lazyloadedLocaleData.Add($"{newTraderId} FirstName", firstName);
-                    lazyloadedLocaleData.Add($"{newTraderId} Nickname", nickName);
-                    lazyloadedLocaleData.Add($"{newTraderId} Location", location);
-                    lazyloadedLocaleData.Add($"{newTraderId} Description", description);
+                    lazyloadedLocaleData!.TryAdd($"{newTraderId} FullName", fullName!);
+                    lazyloadedLocaleData!.TryAdd($"{newTraderId} FirstName", firstName!);
+                    lazyloadedLocaleData!.TryAdd($"{newTraderId} Nickname", nickName!);
+                    lazyloadedLocaleData!.TryAdd($"{newTraderId} Location", location!);
+                    lazyloadedLocaleData!.TryAdd($"{newTraderId} Description", description!);
                     return lazyloadedLocaleData;
                 });
             }
         }
 
-        /// <summary>
-        /// Overwrite the desired traders assorts with the ones provided
-        /// </summary>
-        /// <param name="traderId">Trader to override assorts of</param>
-        /// <param name="newAssorts">new assorts we want to add</param>
         public void OverwriteTraderAssort(string traderId, TraderAssort newAssorts)
         {
             if (!databaseService.GetTables().Traders.TryGetValue(traderId, out var traderToEdit))
             {
-                logger.Warning($"Unable to update assorts for trader: {traderId}, they couldn't be found on the server");
-
+                logger.Warning($"[AllAmmo] Cannot add assorts. Trader {traderId} not found in DB.");
                 return;
             }
 
-            // Override the traders assorts with the ones we passed in
             traderToEdit.Assort = newAssorts;
         }
     }
--- AddTraderWithAssortJson.cs
+++ AddTraderWithAssortJson.cs
@@ -4,6 +4,7 @@ using SPTarkov.Server.Core.Helpers;
 using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Spt.Config;
 using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Routers;
 using SPTarkov.Server.Core.Servers;
 using SPTarkov.Server.Core.Utils;
@@ -12,74 +13,64 @@ using Path = System.IO.Path;
 
 namespace _allAmmo;
 
-// This record holds the various properties for your mod
 public record ModMetadata : AbstractModMetadata
 {
     public override string ModGuid { get; init; } = "com.tiltushkin.allammo";
     public override string Name { get; init; } = "Tiltushkin-AllAmmo";
     public override string Author { get; init; } = "Tiltushkin";
     public override List<string>? Contributors { get; init; } = ["Tiltushkin"];
-    public override SemanticVersioning.Version Version { get; init; } = new("1.2.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("1.3.0");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
-    public override List<string>? Incompatibilities { get; init; } = [""];
+    public override List<string>? Incompatibilities { get; init; } = [];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; } = "https://github.com/Tiltushkin/Tiltushkin-AllAmmo-CSharp/";
     public override bool? IsBundleMod { get; init; } = false;
     public override string? License { get; init; } = "MIT";
 }
 
-/// <summary>
-/// Feel free to use this as a base for your mod
-/// </summary>
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
 public class AddTraderWithAssortJson(
     ModHelper modHelper,
     ImageRouter imageRouter,
     ConfigServer configServer,
     TimeUtil timeUtil,
-    AddCustomTraderHelper addCustomTraderHelper
+    AddCustomTraderHelper addCustomTraderHelper,
+    ISptLogger<AddTraderWithAssortJson> logger
 )
     : IOnLoad
 {
     private readonly TraderConfig _traderConfig = configServer.GetConfig<TraderConfig>();
     private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
 
-
     public Task OnLoad()
     {
-        // A path to the mods files we use below
-        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+        try
+        {
+            var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+            var traderImagePath = Path.Combine(pathToMod, "data/vafelz.jpg");
+            var traderBase = modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "data/base.json");
+            imageRouter.AddRoute(traderBase.Avatar!.Replace(".jpg", ""), traderImagePath);
+            addCustomTraderHelper.SetTraderUpdateTime(_traderConfig, traderBase, timeUtil.GetHoursAsSeconds(1), timeUtil.GetHoursAsSeconds(2));
 
-        // A relative path to the trader icon to show
-        var traderImagePath = Path.Combine(pathToMod, "data/vafelz.jpg");
+            if (!_ragfairConfig.Traders.TryAdd(traderBase.Id, true))
+            {
+                logger.Warning($"[AllAmmo] Trader {traderBase.Id} already in Ragfair config. Possible conflict?");
+            }
 
-        // The base json containing trader settings we will add to the server
-        var traderBase = modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "data/base.json");
+            addCustomTraderHelper.AddTraderWithEmptyAssortToDb(traderBase);
+            addCustomTraderHelper.AddTraderToLocales(traderBase, "VAFELZ", "All Ammo Trader.");
 
-        // Create a helper class and use it to register our traders image/icon + set its stock refresh time
-        imageRouter.AddRoute(traderBase.Avatar.Replace(".jpg", ""), traderImagePath);
-        addCustomTraderHelper.SetTraderUpdateTime(_traderConfig, traderBase, timeUtil.GetHoursAsSeconds(1), timeUtil.GetHoursAsSeconds(2));
+            var assort = modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "data/assort.json");
+            addCustomTraderHelper.OverwriteTraderAssort(traderBase.Id, assort);
 
-        // Add our trader to the config file, this lets it be seen by the flea market
-        _ragfairConfig.Traders.TryAdd(traderBase.Id, true);
+            logger.Info($"[AllAmmo] Trader {traderBase.Nickname} loaded successfully.");
+        }
+        catch (Exception ex)
+        {
+            logger.Error($"[AllAmmo] Critical error loading trader: {ex.Message}");
+            logger.Error(ex.StackTrace!);
+        }
 
-        // Add our trader (with no items yet) to the server database
-        // An 'assort' is the term used to describe the offers a trader sells, it has 3 parts to an assort
-        // 1: The item
-        // 2: The barter scheme, cost of the item (money or barter)
-        // 3: The Loyalty level, what rep level is required to buy the item from trader
-        addCustomTraderHelper.AddTraderWithEmptyAssortToDb(traderBase);
-
-        // Add localisation text for our trader to the database so it shows to people playing in different languages
-        addCustomTraderHelper.AddTraderToLocales(traderBase, "VAFELZ", "All Ammo Trader.");
-
-        // Get the assort data from JSON
-        var assort = modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "data/assort.json");
-
-        // Save the data we loaded above into the trader we've made
-        addCustomTraderHelper.OverwriteTraderAssort(traderBase.Id, assort);
-
-        // Send back a success to the server to say our trader is good to go
         return Task.CompletedTask;
     }
 }
